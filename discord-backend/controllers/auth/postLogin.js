3329053f15c36bea8371eb989474f4d4b9094567
tanveerahmed20 <tanveer.ahmed@brillio.com>
2023-03-20 20:28:40 +0530

    feat: jwt token

--- discord-backend/controllers/auth/postLogin.js
+++ discord-backend/controllers/auth/postLogin.js
@@ -1,4 +1,42 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
 const postLogin = async (req,res)=>{
+
+    try{    
+        const {mail , password} = req.body; 
+        const user = await User.findOne({
+            mail: mail.toLowerCase(),
+        })
+        console.log(user)
+        if(user && ( bcrypt.compare(password, user.password))){
+
+            // successfully logged in so send a new token
+             const token = 'JWT_TOKEN';
+             return res.status(200).json({
+                userDetails : {
+                    mail: user.mail, 
+                    token: token ,
+                    username : user.username
+
+                }
+
+             });
+
+        }
+        return res.status(200).send({
+            message : "Something went wrong . Please try again "
+        });
+
+
+    }
+    catch(err){
+        return res.status(500).send({
+            message : "Something went wrong . Please try again"
+        })
+    }
     res.send('login route');
 }
 
